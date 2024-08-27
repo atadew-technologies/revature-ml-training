e80357fc5f065d243da439515ff2a1411f83e42f
sriram-atadew <111097289+sriram-atadew@users.noreply.github.com>
2024-08-27 09:30:47 -0600

    kubeflow example

--- kfdemo.py
+++ kfdemo.py
@@ -1,5 +1,6 @@
 from kfp import local
 from kfp import dsl
+from kfp import compiler
 
 local.init(runner=local.DockerRunner())
 
@@ -7,16 +8,14 @@ local.init(runner=local.DockerRunner())
 def add(a: int, b: int) -> int:
     return a + b
 
-# run a single component
-task = add(a=1, b=2)
-assert task.output == 3
 
-# or run it in a pipeline
 @dsl.pipeline
 def math_pipeline(x: int, y: int, z: int) -> int:
     t1 = add(a=x, b=y)
     t2 = add(a=t1.output, b=z)
     return t2.output
 
+
 pipeline_task = math_pipeline(x=1, y=2, z=3)
-assert pipeline_task.output == 6
+
+compiler.Compiler().compile(math_pipeline, package_path='pipeline.yaml')
